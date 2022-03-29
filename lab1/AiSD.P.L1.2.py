181ab0f26dab723954f074923a4dc5764f70fd70
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-03-29 17:48:44 +0200

    first commit

---
+++ lab1/AiSD.P.L1.2.py
@@ -0,0 +1,11 @@
+from random import randint
+n = 10
+# matrix = [[randint(0, 1) for _ in range(n)] for _ in range(n)]
+
+matrix = [
+    [0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 0],
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 0, 0, 1],
+]
