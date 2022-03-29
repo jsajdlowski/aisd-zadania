d27fa57a65be351e2ea565d68979f83a9df10e4a
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-03-29 18:07:37 +0200

    splited tests

--- lab1/improvedFindBiggestRectangle_test.py
+++ lab1/improvedFindBiggestRectangle_test.py
@@ -1,21 +1,4 @@
-from findBiggestRectangle import findBiggestRectangle
 from improvedFindBiggestRectangle import improvedFindBiggestRectangle
-
-
-def test_findBiggestRectangle() -> None:
-    assert findBiggestRectangle([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [
-        0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]) == 0
-
-    assert findBiggestRectangle([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [
-                                1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]) == 25
-
-    assert findBiggestRectangle([]) == 0
-
-    assert findBiggestRectangle(
-        [[1, 0, 0, 1, 1, 1], [1, 0, 1, 1, 0, 1], [0, 1, 1, 1, 1, 1], [0, 0, 1, 1, 1, 1]]) == 12
-
-    assert findBiggestRectangle(
-        [[1, 0, 0, 1], [1, 0, 1, 1], [0, 1, 1, 1], [0, 0, 1, 1]]) == 6
 
 
 def test_improvedFindBiggestRectangle() -> None:
