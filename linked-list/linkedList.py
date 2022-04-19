8365e4baaa2e9767cf23843ac91537c090e12ed4
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-04-19 20:14:49 +0200

    rozwiązanie linkedList

--- linked-list/linkedList.py
+++ linked-list/linkedList.py
@@ -1,41 +1,88 @@
+import copy
+
+
 class Node:
-    def __init__(self, k) -> None:
-        self.key = k
-        self.prev = None
-        self.next = None
+    def __init__(self, data: str | None) -> None:
+        self.data = data
+        self.next: None | Node = None
+        self.prev: None | Node = None
 
 
 class LinkedList:
-    def __init__(self) -> None:
-        self.head = None
+    def __init__(self, data=None) -> None:
+        if data == None:
+            self.head: None | Node = None
+        self.head = Node(data)
 
-    def insert(self, x):
-        x.next = self.head
+        self.none = Node(None)
+        self.none.next = self.head
+
+    def prepend(self, node: Node) -> None:
+        node.next = self.head
+
         if self.head != None:
-            self.head.prev = x
-        self.head = x
-        x.prev = None
+            self.head.prev = node
 
-    def search(self, k):
-        x = self.head
-        while x != None and x.key != k:
-            x = x.next
-        return x
+        self.head = node
+        node.prev = self.none
+        self.none.next = node
 
-    def delete(self, x):
-        if x.prev != None:
-            x.prev.next = x.next
-        else:
-            self.head = x.next
-        if x.next != None:
-            x.next.prev = x.prev
+    def listPrint(self) -> None:
+        temp = []
 
-    # def deleteWithGuardian(self, x):
-    #     x.prev.next = x.next
-    #     x.next.prev = x.prev
+        current = self.none
+        while current.next != None:
+            current = current.next
+            temp.append(current.data)
 
-    # def listSearch(self, k):
-    #     x = self.none.next
-    #     while x != None and x.key != k:
-    #         x = x.next
-    #     return x
+        print(temp)
+
+    def search(self, data: str | None) -> Node | None:
+        node = self.none.next
+
+        while node != None and node.data != data:
+            node = node.next
+
+        return node
+
+    def listDelete(self, node: Node) -> None:
+        temp = self.search(node.data)
+        if temp == None:
+            return
+
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
+
+    def uniques(self) -> list[str]:
+        result = []
+
+        current = self.none
+        while current.next != None:
+            current = current.next
+            if current.next not in result:
+                result.append(current.data)
+
+        return result
+
+
+def merge(list1: LinkedList, list2: LinkedList) -> LinkedList | None:
+    if list1.head == None and list2.head == None:
+        return None
+    if list1.head == None:
+        return list2
+    if list2.head == None:
+        return list1
+
+    result = copy.deepcopy(list1)
+
+    current = result.none
+    while current.next != None:
+        current = current.next
+    current.next = list2.head
+
+    list1.head = None
+    list1.none.next = None
+    list2.head = None
+    list2.none.next = None
+
+    return result
