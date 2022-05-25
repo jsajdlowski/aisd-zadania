4be367ab9c0bdbb09befb17b94d86f5bdaa95039
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-05-25 12:27:12 +0200

    fix

--- binary-tree/binaryTree.py
+++ binary-tree/binaryTree.py
@@ -1,6 +1,7 @@
 class Node:
-    def __init__(self, x):
-        self.key = x
+    def __init__(self, key, amount=1):
+        self.key = key
+        self.amount = amount
         self.left = None
         self.right = None
         self.parent = None
@@ -10,7 +11,7 @@ class MyBST:
     def __init__(self):
         self.root = None
 
-    def BSTsearch(self, key):
+    def search(self, key):
         x = self.root
         while x != None and x.key != key:
             if key < x.key:
@@ -19,47 +20,75 @@ class MyBST:
                 x = x.right
         return x
 
-    def BSTinsert(self, z):
-        x = self.root
-        y = None
-        while x != None:
-            y = x
-            if z.key < x.key:
-                x = x.left
+    def insert(self, node):
+        root = self.root
+        father = None
+        while root != None:
+            father = root
+            if node.key < root.key:
+                root = root.left
+            elif node.key > root.key:
+                root = root.right
             else:
-                x = x.right
-
-        z.parent = y
-        if y == None:
-            self.root = z
+                root.amount += 1
+                break
+        node.parent = father
+        if father == None:
+            self.root = node
         else:
-            if z.key < y.key:
-                y.left = z
-            else:
-                y.right = z
+            if node.key < father.key:
+                father.left = node
+            elif node.key > father.key:
+                father.right = node
 
-    def bstMinimum(self, x):
-        while x.left != None:
-            x = x.left
-            return x
-
-    def bstDelete(self, z):
-        if z.left == None and z.right == None:
-            if z == self.root:
-                self.root = None
+    def delete(self, node):
+        if(node.amount > 1):
+            node.amount = node.amount - 1
+        else:
+            if node.left == None:
+                self.transplant(node, node.right)
+            elif node.right == None:
+                self.transplant(node, node.left)
             else:
-                if z == z.parent.left:
-                    z.parent.left = None
-                else:
-                    z.parent.right = None
-        elif z.left != None and z.right != None:
-            y = self.bstMinimum(z.right)
-            z.key = y.key
-            self.bstDelete(y)
+                y = self.minimum(node.right)
+                if y.parent != node:
+                    self.transplant(y, y.right)
+                    y.right = node.right
+                    y.right.parent = y
+                self.transplant(node, y)
+                y.left = node.left
+                y.left.parent = y
+
+    def transplant(self, u, v):
+        if u.parent == None:
+            self.root = v
+        else:
+            if u == u.parent.left:
+                u.parent.left = v
+            else:
+                u.parent.right = v
+        if v != None:
+            v.parent = u.parent
+
+    def minimum(self, node):
+        while node.left != None:
+            node = node.left
+        return node
+
+    def inOrderD(self, x, d, tab=[]):
+        if x == None:
+            return
+        self.inOrderD(x.left, d + 1, tab)
+        tab.append([[x.key, x.amount], d])
+        self.inOrderD(x.right, d + 1, tab)
+        if d == 0:
+            tab.reverse()
+            for i in tab:
+                print(i[1] * 3 * " ", i[0])
 
 
 def printTree(root, level=0):
     if root != None:
         printTree(root.left, level + 1)
-        print(' ' * 4 * level + '-> ' + root.key)
+        print(f"{' ' * 4 * level} -> [{root.key}, {root.amount}]")
         printTree(root.right, level + 1)
--- binary-tree/main.py
+++ binary-tree/main.py
@@ -3,15 +3,21 @@ from binaryTree import Node, MyBST, printTree
 if __name__ == "__main__":
     tree = MyBST()
 
-    [node1, node2, node3, node4, node5, node6, node7] = [Node("jeden"), Node(
-        "dwa"), Node("trzy"), Node("trzy1"), Node("trz"), Node("dwa1"), Node("dwa2")]
+    tree.insert(Node("jeden"))
+    tree.insert(Node("dwa"))
+    tree.insert(Node("trzy"))
+    tree.insert(Node("cztery"))
+    tree.insert(Node("cztery"))
+    tree.insert(Node("jeden"))
+    tree.insert(Node("dwa"))
 
-    tree.BSTinsert(node1)
-    tree.BSTinsert(node2)
-    tree.BSTinsert(node3)
-    tree.BSTinsert(node4)
-    tree.BSTinsert(node5)
-    tree.BSTinsert(node6)
-    tree.BSTinsert(node7)
+    print(tree.search("dwa").key + ", " + str(tree.search("dwa").amount))
 
+    printTree(tree.root)
+
+    tree.delete(tree.search("dwa"))
+    tree.delete(tree.search("cztery"))
+    tree.delete(tree.search("cztery"))
+
+    print()
     printTree(tree.root)
